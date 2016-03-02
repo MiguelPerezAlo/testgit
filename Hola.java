bb5da8b4ef0e6b4efb85eece4ba05c9f891cd837
Miguel Perez <miguelperezalo@gmail.com>
2016-03-02 20:24:58 +0100

    Añadimos una parte del codigo

--- Hola.java
+++ Hola.java
@@ -1,5 +1,6 @@
 public class Hola{
+	static String nombre = "RETRATADO";
 	public static void main(String [] args){
-		System.out.println("Hola " + args[0]);
+		System.out.println("Hola " + nombre);
 	}
 }
